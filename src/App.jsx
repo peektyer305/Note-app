a4eab75821413d0688840f93bddd57a82b2a343c
Masaki Hirabayashi <pektyer305@gmail.com>
2023-12-19 15:16:31 +0900

    Main Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-
+import Sidebar from './components/Sidebar.jsx';
+import Main from "./components/Main.jsx"
 import './App.css';
 
 function App() {
@@ -7,6 +8,8 @@ function App() {
   return (
     <div>
       <p>布団ちゃん</p>
+      <Sidebar />
+      <Main />
     </div>
   )
 }
