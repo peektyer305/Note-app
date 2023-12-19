351afaaf341519b8dd83036eab6ea7e610d5173e
Masaki Hirabayashi <pektyer305@gmail.com>
2023-12-19 21:21:00 +0900

    っっっっっっっd

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import "./Main.css";
 
 const Main = () => {
   return (
-    <div className="Maincontainer">Main</div>
+    <div className="Maincontainer">Maっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっdin</div>
   )
 };
 
