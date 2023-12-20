52f39fad79631880e12fe27c155c5be4e79d3f61
Masaki Hirabayashi <pektyer305@gmail.com>
2023-12-20 17:23:28 +0900

    yfu

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,12 +4,12 @@ const Main = ({activeNote}) => {
   if(!activeNote) return(
     <div>Create or select your note.</div>
   )
-  
+
     return (
     <div className="Maincontainer">
         <section className="main-note-edit">
-            <input type="text" />
-            <textarea id=""placeholder="ノート内容を記入"></textarea>
+            <input type="text" value={activeNote.title}/>
+            <textarea id=""placeholder="ノート内容を記入" value={activeNote.content}></textarea>
         </section>
         <section className="main-note-preview">
             <h1 className="preview-title">{activeNote.title}</h1>
