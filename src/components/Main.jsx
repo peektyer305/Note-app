8e0b51c77f689c863aed051e3b17d63c17487e6e
Masaki Hirabayashi <pektyer305@gmail.com>
2023-12-20 14:12:10 +0900

    Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,27 @@ import "./Main.css";
 
 const Main = () => {
   return (
-    <div className="Maincontainer">vanitas vanitatum, et omnia vanitasddddddddddddddddddddddddddddddddddddddddddddddddddddvanitas vanitatum, et omnia vanitas</div>
+    <div className="Maincontainer">
+        <section className="main-note-edit">
+            <input type="text" />
+            <textarea id=""placeholder="ノート内容を記入"></textarea>
+        </section>
+        <section className="main-note-preview">
+            <h1 className="preview-title">vanitas vanitatum, et omnia vanitas</h1>
+            <div className="markdown-preview">ματαιότης ματαιοτήτων, τὰ πάντα ματαιότης
+
+                    ——七十人訳聖書
+
+                ματαιοτηϲ ματαιοτητων · τα παντα μανταιοτηϲ
+
+                            ——シナイ写本
+
+                ματαιότηϲ ματαιοτήτων τὰ πά-τα ματαιότηϲ
+
+            ——ヴァチカン写本
+            </div>
+        </section>
+    </div>
   )
 };
 
