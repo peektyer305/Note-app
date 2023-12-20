7d0e3228e84c887981cd54ec4ecd5b65ef44e156
Masaki Hirabayashi <pektyer305@gmail.com>
2023-12-20 13:07:44 +0900

    f

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import "./Main.css";
 
 const Main = () => {
   return (
-    <div className="Maincontainer">vanitas vanitatum, et omnia vanitas</div>
+    <div className="Maincontainer">vanitas vanitatum, et omnia vanitasddddddddddddddddddddddddddddddddddddddddddddddddddddvanitas vanitatum, et omnia vanitas</div>
   )
 };
 
